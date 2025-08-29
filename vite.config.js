ad4bbda76c5523a650a3d7ce093a46753604b96d
Rainscarr <113060064+Rainscarr@users.noreply.github.com>
2025-08-29 19:10:30 +0300

    add new domain to file

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['ai-cartoons.onrender.com'] // Добавь эту секцию
+  }
 })
